2ae8dc4b3135d7a073e42191719299384b9f01ba
Wisdom Warmate <wisdomwarmates@gmail.com>
2025-09-22 01:45:38 +0100

    Add 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const size = Number(process.argv[2]);
+
+if (!Number.isInteger(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
